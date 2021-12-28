f2998f160aba005467ee47c4aaececa7f0ab856f
xndrbrgs <aborgescolon@gmail.com>
2021-12-28 15:01:10 -0500

    started js code to remove elements from page once clicked

---
+++ Assets/code/script.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById('start-button');
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    console.log("started");
+    startButton.classList.add("hide");
+}
+
+function nextQuestion() {
+
+}
+
+function answerSelected() {
+
+}
