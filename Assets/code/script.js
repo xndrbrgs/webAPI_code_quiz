9da1ee1f519709ceb546ec05fb47087cf3354aa0
xndrbrgs <aborgescolon@gmail.com>
2021-12-28 15:53:15 -0500

    tied js commands to button elements on html

--- Assets/code/script.js
+++ Assets/code/script.js
@@ -1,13 +1,44 @@
+// Variables List
+
 var startButton = document.getElementById('start-button');
+var questionContainerEle = document.getElementById('question-container');
+var randomQuestions, currentQuesIndex;
+var questionElements = document.getElementById('questions');
+var answerButtonsElements = document.getElementById('buttons');
+
+// Event Listener List
 startButton.addEventListener("click", startGame);
 
+
+// Questions List
+
+var questionsList = [
+    {
+        question: 'Inside which HTML element do we put the JS?',
+        answers: [
+            { text: '<js>', correct: false},
+            { text: '<script>', correct: true}
+        ]
+    }
+]
+
+// Functions List
+
 function startGame() {
-    console.log("started");
+    console.log("Start Quiz");
     startButton.classList.add("hide");
+    randomQuestions = questionsList.sort(() => Math.random() - .5);
+    currentQuesIndex = 0;
+    questionContainerEle.classList.remove("hide");
+    nextQuestion();
+}
+
+function newQuestion(question) {
+
 }
 
 function nextQuestion() {
-
+    newQuestion(randomQuestions[currentQuesIndex]);
 }
 
 function answerSelected() {
